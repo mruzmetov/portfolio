5cab4ddb4eaf9fd441da4b5a2a980084deda3ba0
MOHIRBEK <m.ruzme1ov@gmail.com>
2026-06-21 21:18:43 +0500

    Birinchi qadam boshlandi

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='w-full h-1.5'>
+      Personal Portfolio application
+    </div>
+  )
+}
+
+export default Navbar
