99b88bea7fe14da02d33eaeb733f99cbe1074628
MOHIRBEK <m.ruzme1ov@gmail.com>
2026-06-29 08:05:16 +0500

    Header section

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,19 @@
 import { assets } from '@/assets/assets'
 import Image from 'next/image'
-import React from 'react'
+import React, { useRef } from 'react'
 
 const Navbar = () => {
+
+    const sideMenuRef = useRef();
+
+    const openMenu = () => {
+        sideMenuRef.current.style.transform = 'translateX(-16rem)'
+    }
+
+    const closeMenu = () => {
+        sideMenuRef.current.style.transform = 'translateX(16rem)'
+    }
+
   return (
     <>
     <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
@@ -35,13 +46,27 @@ const Navbar = () => {
                 py-2.5 border border-gray-500 rounded-full ml-4'>Contact 
                 <Image src={assets.arrow_icon}  alt='' className='w-4'/></a>
 
-                <button className='block md:hidden ml-3'>
+                <button className='block md:hidden ml-3' onClick={openMenu}>
                     <Image src={assets.menu_black} alt='' className='w-6'/>
                 </button>
 
             </div>
 
+            {/* -------- mobile menu ---------*/}
 
+            <ul ref = {sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 
+            top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500'>
+
+                <div className='absolute right-6 top-6' onClick={closeMenu}>
+                    <Image src={assets.close_black} alt='' className='w-5 cursor-pointer' />
+                </div>
+
+                <li><a className='font-ovo' onClick={closeMenu} href='#top'>Home</a></li>
+                <li><a className='font-ovo' onClick={closeMenu} href='#about'>About me</a></li>
+                <li><a className='font-ovo' onClick={closeMenu} href='#services'>Services</a></li>
+                <li><a className='font-ovo' onClick={closeMenu} href='#work'>My Work</a></li>
+                <li><a className='font-ovo' onClick={closeMenu} href='#contact'>Contact me</a></li>
+            </ul>
 
         </nav>
     </>
