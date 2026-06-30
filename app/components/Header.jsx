1e318ee15891f07745bc10d0c9907f22831cb2c5
MOHIRBEK <m.ruzme1ov@gmail.com>
2026-06-30 07:54:43 +0500

    Header section for changes

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -5,22 +5,30 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4'>
-        <div>
-            <Image src={assets.profile_img} alt='' className='rounded-full w-32' />
-        </div>
-        <h3 className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-ovo'>Hi! I'm Mohirbek Ruzmetov <Image src={assets.hand_icon}/></h3>
-      <h1 className='text-3xl sm:text-6xl lg:text-[66px] font-ovo'>
-          fullstack web developer from Khorezm
+      <div>
+        <Image src={assets.profile_img} alt='' className='rounded-full w-32'/>
+      </div>
+      <h3 className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-ovo'>
+          Hi! I'm Mohirbek Ruzmetov <Image src={assets.hand_icon} alt='' className='w-7'/>
+        </h3>
+        <h1 className='text-3xl sm:text-6xl lg:text-[66px] font-ovo'>
+          fullstack web developer from Khorezm</h1>
           <p className='max-w-2xl mx-auto font-ovo'>
-            I am a full-stack developer from Khorezm, Uzbekistan. I work as a freelancer with 3 years of experience developing and delivering software products for clients across various industries.
+            I am a full-stack developer from Khorezm, Uzbekistan. I work as a 
+            freelancer with 3 years of experience developing and delivering 
+            software products for clients across various industries.
           </p>
           <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
-            <a href='#contact' className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2'>
-              contact me <Image src={assets.right_arrow_white} alt='' className='w-4'/></a>
-            <a href="/sample-resume.pdf" download className='px-10 py-3 border border-gray-500 rounded-full flex items-center gap-2 bg-white dark:text-black'>
-              my rezume <Image src={assets.download_icon} alt='' className='w-4'/></a>
+            <a href='#contact' 
+            className='px-10 py-3 border border-white rounded-full bg-black text-white
+            flex items-center gap-2'>contact me <Image src={assets.right_arrow_white} alt=''
+            className='w-4'/></a>
+
+            <a href='/rezume.pdf' download
+              className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2'
+            >my rezume <Image src={assets.download_icon} alt=''
+            className='w-4'/></a>
           </div>
-      </h1>
     </div>
   )
 }
--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${outfit.variable} ${ovo.variable} h-full antialiased`}
+      <body className={`${outfit.variable} ${ovo.variable} h-full antialiased 
+      leading-8 overflow-x-hidden`}
       >
         {children}
       </body>
